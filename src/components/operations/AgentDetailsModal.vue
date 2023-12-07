f376abb5e076292a0744c173779e8207be8f4443
JamieScottC <scott.jams@gmail.com>
2023-12-07 11:33:10 -0500

    remove agent links panel

--- src/components/operations/AgentDetailsModal.vue
+++ src/components/operations/AgentDetailsModal.vue
@@ -182,69 +182,6 @@ function saveAgent() {
                       tr
                           th.has-text-right Peer-toPeer Proxy Chains
                           td {{ (selectedAgent.proxy_chain && selectedAgent.proxy_chain.length) ? selectedAgent.proxy_chain.join(', ') : 'Not using P2P agents to reach C2.' }}
-            button.accordion(@click="showActions = !showActions") Agent Links
-            .panel(:style="{ maxHeight: showActions ? '700px' : '0px' }")
-              table
-                  col(width="30%")
-                  col(width="70%")
-                  tbody
-                      tr
-                          th.has-text-right Status
-                          td 
-                              span(:class="{ 'has-text-warning': getAgentStatus(selectedAgent) === 'dead', 'has-text-success': getAgentStatus(selectedAgent) === 'alive', 'has-text-info': getAgentStatus(selectedAgent) === 'pending kill' }") {{ getAgentStatus(selectedAgent) }}
-                              span , 
-                              span(:class="{ 'has-text-warning': !selectedAgent.trusted, 'has-text-success': selectedAgent.trusted }") {{ selectedAgent.trusted ? 'trusted' : 'untrusted' }}
-                      tr
-                          th.has-text-right Paw
-                          td {{ selectedAgent.paw }}
-                      tr
-                          th.has-text-right Host
-                          td {{ `${selectedAgent.host} (${selectedAgent.host_ip_addrs ? selectedAgent.host_ip_addrs.join(', ') : ''})` }}
-                      tr(v-if="selectedAgent.display_name")
-                          th.has-text-right Display Name
-                          td {{ selectedAgent.display_name }}
-                      tr
-                          th.has-text-right Username
-                          td {{ selectedAgent.username }}
-                      tr
-                          th.has-text-right Privilege
-                          td {{ selectedAgent.privilege }}
-                      tr
-                          th.has-text-right Last Seen
-                          td {{ new Date(selectedAgent.last_seen).toLocaleString() }}
-                      tr
-                          th.has-text-right Created
-                          td {{ new Date(selectedAgent.created).toLocaleString() }}
-                      tr
-                          th.has-text-right Architecture
-                          td {{ selectedAgent.architecture }}
-                      tr
-                          th.has-text-right Platform
-                          td {{ selectedAgent.pid }}
-                      tr
-                          th.has-text-right PID
-                          td {{ selectedAgent.pid }}
-                      tr
-                          th.has-text-right PPID
-                          td {{ selectedAgent.ppid }}
-                      tr
-                          th.has-text-right Executable Name
-                          td {{ selectedAgent.exe_name }}
-                      tr
-                          th.has-text-right Location
-                          td {{ selectedAgent.location }}
-                      tr
-                          th.has-text-right Executors
-                          td {{ selectedAgent.executors ? selectedAgent.executors.join(", ") : '' }}
-                      tr(v-if="selectedAgent.host_ip_addrs")
-                          th.has-text-right Host IP Addresses
-                          td {{ selectedAgent.host_ip_addrs ? selectedAgent.host_ip_addrs.join(", ") : '' }}
-                      tr
-                          th.has-text-right Peer-to-Peer Proxy Receivers
-                          td {{ (selectedAgent.proxy_receivers && Object.keys(selectedAgent.proxy_receivers).length) ? Object.keys(selectedAgent.proxy_receivers) : 'No local P2P proxy receivers active.' }}
-                      tr
-                          th.has-text-right Peer-toPeer Proxy Chains
-                          td {{ (selectedAgent.proxy_chain && selectedAgent.proxy_chain.length) ? selectedAgent.proxy_chain.join(', ') : 'Not using P2P agents to reach C2.' }}
         footer.modal-card-foot.is-flex.is-justify-content-flex-end 
             button.button(@click="modals.operations.showAgentDetails = false") Close
             button.button.is-danger.is-outlined(@click="agentStore.killAgent($api, selectedAgent.paw); modals.operations.showAgentDetails = false;")
